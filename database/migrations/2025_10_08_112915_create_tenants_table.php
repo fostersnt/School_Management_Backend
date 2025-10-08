bb4d0ee664a0d503ff670a28a50ff3e98af825b1
Foster Amponsah <foster.asante@gwosevo.com>
2025-10-08 11:39:42 +0000

    Update

--- database/migrations/2025_10_08_112915_create_tenants_table.php
+++ database/migrations/2025_10_08_112915_create_tenants_table.php
@@ -6,13 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('tenants', function (Blueprint $table) {
             $table->id();
+            $table->string('host');
+            $table->string('db_name');
+            $table->string('db_username');
+            $table->string('db_password');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
