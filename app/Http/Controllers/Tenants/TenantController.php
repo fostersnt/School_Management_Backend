94c114a0fb81fc05651d03085902c92c6b3f645f
Foster Amponsah <foster.asante@gwosevo.com>
2025-10-08 14:07:21 +0000

    Update

--- app/Http/Controllers/Tenants/TenantController.php
+++ app/Http/Controllers/Tenants/TenantController.php
@@ -2,14 +2,23 @@
 
 namespace App\Http\Controllers\Tenants;
 
+use App\Helpers\General;
 use App\Http\Controllers\Controller;
 use App\Models\Tenant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TenantController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $tenants = Tenant::all();
+        try {
+            $tenants = Tenant::all();
+            return General::successRequestResponse($tenants, "Tenants data");
+        } catch (\Throwable $th) {
+            $url = $request->getHost();
+            Log::info("TENANT RETRIEVAL === [URL === $url] " . $th->getMessage());
+            return General::failedRequestResponse([], "Error occurred when retrieving tenants data");
+        }
     }
 }
