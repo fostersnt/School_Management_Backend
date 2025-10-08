d489f876a8a8d982a1142a9dae2ab21d44ea3465
Foster Amponsah <foster.asante@gwosevo.com>
2025-10-08 11:47:28 +0000

    Update

--- app/Http/Middleware/TenantIdentification.php
+++ app/Http/Middleware/TenantIdentification.php
@@ -6,6 +6,7 @@ use App\Models\Tenant;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class TenantIdentification
@@ -25,11 +26,16 @@ class TenantIdentification
 
         $tenant = Tenant::where('subdomain', $subdomain)->first();
 
+        Log::info("TENANT === " . json_encode($tenant));
+
         if (!$tenant) {
             abort(404, 'Tenant not found');
+        Log::info("TENANT NOT FOUND === " . json_encode($tenant));
         }
 
         $this->switchDatabaseConnection($tenant);
+
+        Log::info("TENANT SWITCHED === " . json_encode($tenant));
 
         return $next($request);
     }
