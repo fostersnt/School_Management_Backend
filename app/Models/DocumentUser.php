2c920e133a9971ba61012d73355e6d09a560797f
Foster Amponsah <foster.asante@gwosevo.com>
2025-10-23 11:01:01 +0000

    Update

---
+++ app/Models/DocumentUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DocumentUser extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'document_id',
+        'user_id',
+        'uploaded_by'
+    ];
+}
