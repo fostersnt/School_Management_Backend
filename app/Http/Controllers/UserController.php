e7d4b7ac3cc13e1587710177fea586119cb6be57
Foster Amponsah <foster.asante@gwosevo.com>
2025-10-23 11:23:37 +0000

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function create(Request $request)
     {
-        
+        $validator = Validator::make($request->all(), [
+            'name'              =>  'required|string',
+            'email'             =>  'required|string|email',
+            'password'          =>  'required|string|alpha_num',
+            'phone'             =>  'required|string',
+            'role_id'           =>  'required|exists:roles,id',
+            'profile_picture'   =>  'required|mimes:jpg,png,pdf|max:10240'
+        ]);
     }
 }
